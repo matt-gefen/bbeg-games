bdc57d3c3129048e9692853dad7386b6b38798f5
Matt Gefen <matt.gefen@gmail.com>
2025-05-08 16:28:13 -0600

    feat: add placeholder for landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,28 @@
+/* eslint-disable @next/next/no-img-element */
 // import Image from "next/image";
+import { Flex, Text } from "@radix-ui/themes";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        BBEG
+        <Flex direction="column" gap="3" align="center">
+          <Text size="7">Whats up</Text>
+          <Text size="9">BIG BAD EVIL GUYS</Text>
+          <Flex gap="2">
+            <img
+              src="https://1u4j97n0laxnso2y.public.blob.vercel-storage.com/BBEG/bbeg-UARPud147dabpXiM5KLyrPgV8PLqlK.jpg"
+              alt="Bold typography"
+              style={{
+                display: "block",
+                objectFit: "cover",
+                width: "100%",
+                height: 500,
+              }}
+            />
+          </Flex>
+        </Flex>
       </main>
     </div>
   );
